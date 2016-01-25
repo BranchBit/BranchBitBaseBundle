bd3954a71c78bcad7aebda926aed455bcbb9b887
whitewhidow <iwonderwhy5@hotmail.com>
2016-01-25 15:25:54 +0100

    subtree command via git protocol

--- subtree.sh
+++ subtree.sh
@@ -1,4 +1,4 @@
 git subsplit init https://github.com/BranchBit/BranchBitBaseBundle
-git subsplit publish --heads="master" src/BBIT/DoctrineExtensions:https://github.com/BranchBit/DoctrineExtensions.git
-git subsplit publish --heads="master" src/BBIT/SqsCommandQueueBundle:https://github.com/BranchBit/SqsCommandQueueBundle.git
+git subsplit publish --heads="master" src/BBIT/DoctrineExtensions:git@github.com:BranchBit/DoctrineExtensions.git
+git subsplit publish --heads="master" src/BBIT/SqsCommandQueueBundle:git@github.com:BranchBit/SqsCommandQueueBundle.git
 rm -rf .subsplit/
